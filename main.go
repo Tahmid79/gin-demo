de8629e0aae3849738281af645aecb8a25619856
Tahmid Farhan <tahmidfarhan12@gmail.com>
2022-03-08 12:06:17 +0600

    echo

--- main.go
+++ main.go
@@ -2,20 +2,22 @@ package main
 
 import (
 	"log"
+	"net/http"
 
-	"github.com/gin-gonic/gin"
+	"github.com/labstack/echo"
 )
 
 func main() {
-	gin.SetMode(gin.ReleaseMode)
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	// Create a new instance of Echo
+	e := echo.New()
 
-	if err := r.Run("localhost:5000"); err != nil {
-		log.Panicf(err.Error())
+	e.GET("/tasks", func(c echo.Context) error { return c.JSON(200, "GET Tasks") })
+	e.PUT("/tasks", func(c echo.Context) error { return c.JSON(200, "PUT Tasks") })
+	e.DELETE("/tasks/:id", func(c echo.Context) error { return c.JSON(200, "DELETE Task "+c.Param("id")) })
+
+	// Start as a web server
+	if err := e.Start(":3000"); err != http.ErrServerClosed {
+		log.Fatal(err)
 	}
+
 }
